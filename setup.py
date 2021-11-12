56b1eb0fd69bf3de9cc69fbd3b352c70cc088b86
unknown <savasavelck@mail.ru>
2021-11-12 22:20:38 +0300

    Experementing

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="WikipediaQA",
+    version="0.1.0",
+    author="Savelov Dmitry",
+    author_email="savasavelck@mail.ru",
+    # description="A AsyncIO-based module for interacting with the Mediawiki API using aiohttp",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url=r"https://github.com/JosephThePatrician/WikipediaQA",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    install_requires=requirements,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)
